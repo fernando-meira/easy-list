02f0303e4ed0322165f732d5a08c3aa910327215
Fernando Meira <f.lasso.h@gmail.com>
2024-12-25 20:04:08 -0300

    feat: Improvement in new product form.

--- src/components/new-product-form-modal.tsx
+++ src/components/new-product-form-modal.tsx
@@ -33,9 +33,10 @@ export function NewProductFormModal() {
 
     console.log({
       name,
+      unit,
       price,
       quantity,
-      unit,
+      addToCart,
     });
   };
 
@@ -64,8 +65,8 @@ export function NewProductFormModal() {
         </DialogHeader>
 
         <form onSubmit={addOrEditProduct} className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="name">Nome do produto</Label>
+          <div className="col-span-2 space-y-2">
+            <Label htmlFor="name">Produto</Label>
 
             <Input
               id="name"
@@ -77,56 +78,60 @@ export function NewProductFormModal() {
             />
           </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="price">Preço (opcional)</Label>
+          <div className="flex gap-2">
+            <div className="space-y-2">
+              <Label htmlFor="price">Preço (opcional)</Label>
 
-            <Input
-              id="price"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              placeholder="Preço"
-              step="0.01"
-            />
-          </div>
-
-          <div className="space-y-2">
-            <Label htmlFor="quantity">Quantidade/Peso (opcional)</Label>
-
-            <div className="flex gap-2">
               <Input
-                id="quantity"
+                id="price"
+                step="0.01"
                 type="number"
-                value={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-                placeholder={unit === "unit" ? "Quantidade" : "Peso"}
-                className="flex-grow"
+                value={price}
+                placeholder="Preço"
+                onChange={(e) => setPrice(e.target.value)}
               />
-
-              <Select value={unit} onValueChange={(value: "unit" | "grams") => setUnit(value)}>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Selecione a quantidade" />
-                </SelectTrigger>
-
-                <SelectContent>
-                  <SelectItem value="unit">Unidade</SelectItem>
-
-                  <SelectItem value="kg">Kg</SelectItem>
-
-                  <SelectItem value="grams">Gramas</SelectItem>
-                </SelectContent>
-              </Select>
             </div>
 
-            {unit === "grams" && quantity && (
+            <div className="space-y-2">
+              <Label htmlFor="quantity">Quantidade/Peso</Label>
+
+              <div className="flex gap-2">
+                <Input
+                  id="quantity"
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                  placeholder={unit === "unit" ? "Quantidade" : "Peso"}
+                  className="flex-grow"
+                />
+
+                <Select value={unit} onValueChange={(value: "unit" | "grams") => setUnit(value)}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Selecione a quantidade" />
+                  </SelectTrigger>
+
+                  <SelectContent>
+                    <SelectItem value="unit">Unidade</SelectItem>
+
+                    <SelectItem value="kg">Kg</SelectItem>
+
+                    <SelectItem value="grams">Gramas</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {unit === "grams" && (
               <p className="text-sm text-muted-foreground mt-1">
                 O calculo do preço ser&aacute; feito com base no peso em gramas
               </p>
             )}
 
-            {unit === "kg" && quantity && (
+            {unit === "kg" && (
               <p className="text-sm text-muted-foreground mt-1">
-                O calculo do preço ser&aacute; feito com base no peso em quilos
+                O cálculo do preço ser&aacute; feito com base no peso em quilos
               </p>
             )}
           </div>
