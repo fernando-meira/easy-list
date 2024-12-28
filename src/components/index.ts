0d6cf3137a6dc8596c4e5fc1846f5dc8bced6476
Fernando Meira <f.lasso.h@gmail.com>
2024-12-28 01:50:56 -0300

    feat: Separated ProductListHeader in a new component.

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { ProductList } from './ProductList';
 export { NewProductForm } from './NewProductForm';
 export { AddProductModal } from './AddProductModal';
 export { AddProductDrawer } from './AddProductDrawer';
+export { ProductListHeader } from './ProductListHeader';
