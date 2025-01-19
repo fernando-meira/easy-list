c29f296e6ebd5c9e7430d1a88b836746ced933d1
Fernando Meira <f.lasso.h@gmail.com>
2025-01-19 03:52:53 -0300

    feat: Created HomeLoading component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 'use client';
 
 import { useAuth } from '@/hooks/useAuth';
-import { Skeleton } from '@/components/ui/skeleton';
-import { CategoryList, Footer, Header } from '@/components';
+import { CategoryList, Footer, Header, HomeLoading } from '@/components';
 
 export default function Home() {
   const { status } = useAuth(true);
 
   if (status === 'loading') {
     return (
-      <div className="flex flex-col p-4 max-w-3xl">
-        <div className="flex items-center gap-2 animate-pulse">
-          <Skeleton className="h-9 w-28" />
-
-          <Skeleton className="h-9 w-9" />
-        </div>
-
-        <div className="flex items-center space-x-4 mt-4">
-          <div className="space-y-2 w-full">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <Skeleton key={index} className="h-9 w-full" />
-            ))}
-          </div>
-        </div>
-      </div>
+      <HomeLoading />
     );
   }
 
