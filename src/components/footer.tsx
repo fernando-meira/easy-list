48a3e6a95b9bf64f670437a5d9966106cccdc052
Fernando Meira <f.lasso.h@gmail.com>
2025-04-28 18:22:20 -0300

    feat: Adjusted new button placement.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { Skeleton } from './ui/skeleton';
 import { PagesEnum } from '@/types/enums';
 import { convertToCurrency } from '@/utils';
 import { calculateTotalValue } from '@/utils';
 import { usePathname } from 'next/navigation';
 import { NewProductForm } from './new-product-form';
 import { useProducts } from '@/context/ProductContext';
-import { useCategories } from '@/context/CategoryContext';
 import { NewCategoryDrawer } from './new-category-drawer';
 
 export function Footer() {
   const pathname = usePathname();
-  const { isLoadingCategories } = useCategories();
   const { products, allProductsWithoutPrice, allProductsInCartWithoutPrice } = useProducts();
 
   const isHomePage = pathname === PagesEnum.home;
@@ -31,17 +28,13 @@ export function Footer() {
       }
 
       <div className="flex items-center gap-2">
-        {isLoadingCategories ? (
-          <div className="flex items-center gap-2 animate-pulse">
-            <Skeleton className="h-9 w-28" />
-          </div>
-        ) : (
-          <>
-            {!isHomePage ? (
-              <NewProductForm />
-            ): <NewCategoryDrawer />}
-          </>
-        )}
+        <>
+          {!isHomePage ? (
+            <NewProductForm />
+          ) : (
+            <NewCategoryDrawer />
+          )}
+        </>
       </div>
     </footer>
   );
