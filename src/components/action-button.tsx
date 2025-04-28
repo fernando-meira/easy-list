3c71dc647787568189738ec0314a5c61e11cc6a0
Fernando Meira <f.lasso.h@gmail.com>
2025-04-28 18:09:16 -0300

    feat: Changed button style.

---
+++ src/components/action-button.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import { CirclePlus } from 'lucide-react';
+import { Button, ButtonProps } from './ui/button';
+
+interface ButtonActionProps extends ButtonProps {
+  text: ReactNode;
+}
+
+export function ActionButton({ text, ...rest }: ButtonActionProps) {
+  return (
+    <Button {...rest} className='w-full p-6 text-primary rounded-t-sm bg-transparent font-bold shadow-primary'><CirclePlus />{text}</Button>
+  );
+}
