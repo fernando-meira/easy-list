5cd172d42e352056b6b81d176c0fbb39e9bb6edc
Fernando Meira <f.lasso.h@gmail.com>
2026-06-06 14:59:59 -0300

    feat: add Firebase Admin setup

---
+++ src/lib/firebase-admin.ts
@@ -0,0 +1,30 @@
+import 'server-only';
+
+import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const projectId = process.env.AUTH_FIREBASE_PROJECT_ID;
+const clientEmail = process.env.AUTH_FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+if (!projectId) {
+  throw new Error('Please define AUTH_FIREBASE_PROJECT_ID inside .env.local');
+}
+
+if (!process.env.FIRESTORE_EMULATOR_HOST && (!clientEmail || !privateKey)) {
+  throw new Error('Please define AUTH_FIREBASE_CLIENT_EMAIL and AUTH_FIREBASE_PRIVATE_KEY inside .env.local');
+}
+
+const app = getApps()[0] ?? initializeApp(
+  process.env.FIRESTORE_EMULATOR_HOST
+    ? { projectId }
+    : {
+        credential: cert({
+          projectId,
+          clientEmail,
+          privateKey,
+        }),
+      }
+);
+
+export const firestore = getFirestore(app);
